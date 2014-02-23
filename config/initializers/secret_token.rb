bc1e4247a99deb531bdeef987ebd1ce845214bca
Blake Rego <blake.rego@gmail.com>
2014-02-23 15:06:26 -0500

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Diner::Application.config.secret_key_base = 'db0920a2a74eda2bdf89b801ac143d9c11738f895e99889d70a26b61cb7a758fb8fc4f2befe3693e199ebe7bce12823ebbd2900d30e0147de6d4f066c220ae9e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Diner::Application.config.session_store :cookie_store, key: '_Diner_session'
