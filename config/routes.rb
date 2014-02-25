f992bed9ab95934a62f8f5a347cf8c088b0a6a34
Blake Rego <blake.rego@gmail.com>
2014-02-24 19:39:46 -0500

    fixing routing error.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Diner::Application.routes.draw do
   devise_for :users
   root 'home#index'
-  match 'auth/:provider/callback' => 'sessions#create'
+  get 'auth/:provider/callback' => 'sessions#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
