4ad6d1a5343785cfd83c317e65dfd66b0db93833
Blake Rego <blake.rego@gmail.com>
2014-02-26 22:26:28 -0500

    removing columns.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140227024216) do
+ActiveRecord::Schema.define(version: 20140227032438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,6 @@ ActiveRecord::Schema.define(version: 20140227024216) do
     t.integer  "user_id"
     t.string   "provider"
     t.string   "uid"
-    t.string   "index"
-    t.string   "create"
-    t.string   "destroy"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
