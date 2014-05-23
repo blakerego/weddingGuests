5a0b707d9af49c787fbbe22a3348bdcd5ea39df6
Blake Rego <blake.rego@gmail.com>
2014-05-22 23:40:56 -0400

    after a user is created, generate a code for them.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :authentications
+  after_create :assign_code
 
   def apply_omniauth(omniauth)
     self.email = omniauth['info']['email'] if email.blank?
@@ -12,6 +13,13 @@ class User < ActiveRecord::Base
 
   def password_required?
     (authentications.empty? || !password.blank?) && super
+  end
+
+
+
+  def assign_code
+    code = (0...6).map { ('A'..'Z').to_a[rand(26)] }.join
+    self.update_attributes(:code => code)
   end
 
 end
