41344fa92b5de62a46e36e9696ddd352db1f9dfa
Blake Rego <blake.rego@gmail.com>
2014-05-23 18:10:38 -0400

    updating readme.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :authentications
-  after_create :assign_code
 
   def apply_omniauth(omniauth)
     self.email = omniauth['info']['email'] if email.blank?
@@ -13,11 +12,6 @@ class User < ActiveRecord::Base
 
   def password_required?
     (authentications.empty? || !password.blank?) && super
-  end
-
-  def assign_code
-    code = (0...6).map { ('A'..'Z').to_a[rand(26)] }.join
-    self.update_attributes(:code => code)
   end
 
 end
