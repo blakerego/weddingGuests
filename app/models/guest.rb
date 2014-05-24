8cb852f9a39fa1c428d51e8effe171fb0e0fd5bb
Blake Rego <blake.rego@gmail.com>
2014-05-24 08:50:48 -0400

    moving the code yet again. this time from guest to rsvp. guests can share codes via the rvsp.

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,11 +1,3 @@
 class Guest < ActiveRecord::Base
-
-  after_create :assign_code
   belongs_to :rsvp
-
-  def assign_code
-    code = (0...6).map { ('A'..'Z').to_a[rand(26)] }.join
-    self.update_attributes(:code => code)
-  end
-
 end
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,3 +1,11 @@
 class Rsvp < ActiveRecord::Base
   has_many :guests
+
+  after_create :assign_code
+
+  def assign_code
+    code = (0...6).map { ('A'..'Z').to_a[rand(26)] }.join
+    self.update_attributes(:code => code)
+  end
+
 end
